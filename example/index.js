610de2d74ac7fd66ad49223849919a18847cfbda
kevinRoomies <65670017+kevinRoomies@users.noreply.github.com>
2021-11-29 18:28:37 +0100

    Update index.js

--- example/index.js
+++ example/index.js
@@ -6,6 +6,7 @@ const app = express()
 app.use(
   queryParser({
     parseNull: true,
+    parseUndefined: true,
     parseBoolean: true,
     parseNumber: true,
   })
@@ -17,7 +18,7 @@ app.get('/', (req, res) => {
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!')
-  console.log('Try http://localhost:3000/?a=null&b=true&c[d]=null')
-  console.log('Try http://localhost:3000/?a[0]=null&a[1]=true')
+  console.log('Try http://localhost:3000/?a=null&b=undefined&c=true&d[e]=null')
+  console.log('Try http://localhost:3000/?a[0]=null&a[1]=undefined&a[2]=true')
   console.log('Try http://localhost:3000/?a[0]=3.1415926&a[1]=3&a[2]=A.3')
 })
